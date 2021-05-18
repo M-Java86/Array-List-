663230908fac460853bae7716185d20b8fcb6808
M-java86 <mpythondata@outlook.com>
2021-05-18 15:15:02 -0400

    MO: ADDED ArraryList to Runner.java

---
+++ src/com/qa/java/Runner.java
@@ -0,0 +1,23 @@
+package com.qa.java;
+
+import java.util.ArrayList;
+
+// Class Scope
+public class Runner {
+	
+	public static void main(String[]  args) {
+		
+		int[] primaryArray = new int[6];
+				ArrayList<Integer> array = new ArrayList<>();
+}           
+	           //array.add(5);
+	           //array.add(5);
+	          // array.add(5);
+	           
+	          // for(int = 0; x array.size(); x++) {
+	        	// System.out.println(array.get));
+	           }
+	           
+	          // System.out.println( " Size of the ArrayList" + array.size) );
+
+//}
